e4f61ece525f5d48b9a6fa1830d5874732da1aa5
hentaimik <59136177+13582351091@users.noreply.github.com>
2025-01-13 14:08:43 +0800

    main to widget

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-123
+build_runner: ^2.3.0
+
+/Users/rogerxavier/develop/enteriprise_develop/flutter/bin/cache/dart-sdk/dart
+$HOME/.pub-cache/bin/widgetbook widgetbook路径
+
+/Users/rogerxavier/develop/enteriprise_develop/flutter/bin/cache/dart-sdk/bin/dart run build_runner build -d
+
+
+
+/Users/rogerxavier/develop/enteriprise_develop/flutter/bin/flutter --no-color build web  --target=lib/app.widgetbook.dart
+
+$HOME/.pub-cache/bin/widgetbook cloud build push --api-key 8083be51a72a19d0ff3334470e35bb22eea7ca39a1fe2d3a5379f9b22cc8d0a5 --repository https://github.com/Yunbowu/widgetbook_simple --actor rogerxavier
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,123 @@
+// ignore_for_file: depend_on_referenced_packages
+
 import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_in_flutter_course/widgets/button.dart';
+
+import 'components/login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const HotReload());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HotReload extends StatelessWidget {
+  const HotReload({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return Widgetbook.material(
+      addons: [
+        // // 主题
+        buildMaterialThemeAddon(),
+
+        // // 字体大小
+        buildTextScaleAddon(),
+      ],
+      directories: [
+        // 基础组件
+        buildWidgetbookCategory(),
+
+        // 业务组件
+        buildWidgetbookCategory2(),
+      ],
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  TextScaleAddon buildTextScaleAddon() {
+    return TextScaleAddon(
+      scales: [1.0, 1.25, 1.5, 1.75, 2],
+      // defaultScale: 1.0
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  MaterialThemeAddon buildMaterialThemeAddon() {
+    return MaterialThemeAddon(
+      // defaultTheme: WidgetbookTheme(name: "light", data: ThemeData.light()),
+        themes: [
+          WidgetbookTheme(name: "dark", data: ThemeData.dark()),
+          WidgetbookTheme(name: "light", data: ThemeData.light()),
+        ]);
+  }
+
+  WidgetbookCategory buildWidgetbookCategory2() {
+    return WidgetbookCategory(
+      name: '业务组件',
+      children: [
+        WidgetbookComponent(
+          name: '系统常用',
+          useCases: [
+            WidgetbookUseCase(
+              name: "登录界面",
+              builder: (BuildContext context) {
+                return LoginForm(
+                  title: context.knobs.string(
+                    label: '标题 [title]',
+                    initialValue: '用户登录',
+                  ),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            WidgetbookUseCase(
+              name: "注册界面",
+              builder: (BuildContext context) {
+                return LoginForm(
+                  title: context.knobs.string(
+                    label: '标题 [title]',
+                    initialValue: '用户注册',
+                  ),
+                );
+              },
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
+    );
+  }
+
+  WidgetbookCategory buildWidgetbookCategory() {
+    return WidgetbookCategory(
+      name: '公共组件',
+      children: [
+        WidgetbookComponent(
+          name: '按钮',
+          useCases: [
+            WidgetbookUseCase.center(
+              name: "红色背景",
+              child: MyElevatedButton(
+                onPressed: () => print("Button pressed"),
+                text: "Click me",
+                icon: Icons.arrow_forward,
+                buttonColor: Colors.red,
+                borderRadius: 16.0,
+                height: 60.0,
+                width: double.infinity,
+              ),
+            ),
+            WidgetbookUseCase.center(
+              name: "蓝色色背景",
+              child: MyElevatedButton(
+                onPressed: () => print("Button pressed"),
+                text: "Click me",
+                icon: Icons.arrow_forward,
+                buttonColor: Colors.blue,
+                borderRadius: 16.0,
+                height: 60.0,
+                width: double.infinity,
+              ),
+            )
+          ],
+        ),
+      ],
     );
   }
 }
